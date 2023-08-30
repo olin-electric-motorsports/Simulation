055af573ca70b479afb6b1fb6e2418229fc76e9d
richardli03 <rli@olin.edu>
2023-08-30 15:25:30 -0400

    add complete_dict attribute + set up infrastructure for finding compliant values

--- libraries/tire_sim/tires.py
+++ libraries/tire_sim/tires.py
@@ -24,6 +24,7 @@ from scipy.io import loadmat
 import numpy as np
 import matplotlib.pyplot as plt
 from enum import Enum, auto
+from pprint import pprint
 
 
 class Stats(Enum):
@@ -46,25 +47,44 @@ class Stats(Enum):
 
 TOLERANCE = {
     Stats.CAMBER_ANGLE: 0.5,
-    Stats.NORMAL_FORCE: 200,
+    Stats.NORMAL_FORCE: 500,
     Stats.PRESSURE: 25,
 }
 
 
 class TireParser:
     def __init__(self, path_to_summary_table, path_to_data_spreadsheet):
+        """
+        Initialize all of the index dictionaries given a data spreadsheet.
+
+        :param path_to_summary_table: _description_
+        :type path_to_summary_table: _type_
+        :param path_to_data_spreadsheet: _description_
+        :type path_to_data_spreadsheet: _type_
+        """
         summary_data = pd.read_excel(path_to_summary_table)
         raw_data = loadmat(path_to_data_spreadsheet)
+
+        self.tireid = raw_data["tireid"]
+        print(f"Tire ID being queried: {self.tireid}")
 
         self.normal_force_data = raw_data["FZ"]
         self.pressure_data = raw_data["P"]
         self.camber_data = raw_data["IA"]
 
-        self.camber_indices = self.find_indices(self.camber_data, Stats.CAMBER_ANGLE)
-        self.normal_force_indices = self.find_indices(
+        self.camber = self.find_indices(self.camber_data, Stats.CAMBER_ANGLE)
+        self.normal_force = self.find_indices(
             self.normal_force_data, Stats.NORMAL_FORCE
         )
-        self.pressure_indices = self.find_indices(self.pressure_data, Stats.PRESSURE)
+        # pprint(self.normal_force)
+        self.pressure = self.find_indices(self.pressure_data, Stats.PRESSURE)
+
+        self.complete_dict = {
+            Stats.CAMBER_ANGLE.name: self.camber,
+            Stats.NORMAL_FORCE.name: self.normal_force,
+            Stats.PRESSURE.name: self.pressure,
+        }
+        pprint(self.complete_dict)
 
     def find_indices(self, data_column, key):
         """
@@ -80,17 +100,47 @@ class TireParser:
         :rtype: _type_
         """
 
-        unique = []
+        unique = {}
         curr_val = data_column[0]
         for i, val in enumerate(data_column):
             if abs(val - curr_val) > TOLERANCE[key]:
-                unique.append(i)
+                # FIGURE OUT HOW TO MAKE THIS AN ESTIMATED INSTEAD OF A SPECIFIC VAL
+                unique[float(curr_val)] = i
                 # print(val, curr_val)
                 curr_val = val
 
         return unique
 
-    def show_data(self, data, indices, title=None):
+    def find_compliant_values(self, config_dict):
+        """
+        based on a specified set of values, find the indices that satisfy all of those conditions
+
+        :param config_dict: _description_
+        :type config_dict: _type_
+        :param complete_dict: _description_
+        :type complete_dict: _type_
+        """
+
+        # self.complete_dict
+
+    def query(self, **kwargs):
+        """
+        Given a set of keyword arguments, find the data that satisfies all of those conditions
+
+        :param kwargs: _description_
+        :type kwargs: _type_
+        """
+        config = {}
+        for arg in kwargs:
+            # NAME OF SETTING
+            setting = list(kwargs[arg].keys())[0]
+            value = list(kwargs[arg].values())[0]
+            config[setting] = value
+
+        self.find_compliant_values(config)
+        pass
+
+    def show_data(self, data, index_dict, title=None):
         """
         Show a set of data with certain indices highlighted
 
@@ -102,24 +152,27 @@ class TireParser:
         :type title: _type_, optional
         """
         plt.plot(data)
-        for index in indices:
-            plt.axvline(x=index, color="red")
+        # this iterates through the keys, which are indexes
+        for index in index_dict:
+            plt.axvline(x=index_dict[index], color="red")
 
         plt.title(title)
         plt.show()
 
 
 if __name__ == "__main__":
-    tire_parser = TireParser(
+    parser = TireParser(
         path_to_summary_table="assets/tire_data/ttc_data.xls",
         path_to_data_spreadsheet="assets/tire_data/B1464run19.mat",
     )
 
-    pattern = tire_parser.normal_force_indices
-    tire_parser.show_data(tire_parser.camber_data, pattern, "camber on camber")
-    tire_parser.show_data(
-        tire_parser.normal_force_data,
-        pattern,
-        "camber on normal force",
-    )
-    tire_parser.show_data(tire_parser.pressure_data, pattern, "camber on pressure")
+    parser.query(val1={Stats.CAMBER_ANGLE: 2.004})
+
+    pattern = parser.camber
+    # parser.show_data(parser.camber_data, pattern, "camber on camber")
+    # parser.show_data(
+    #     parser.normal_force_data,
+    #     pattern,
+    #     "camber on normal force",
+    # )
+    # parser.show_data(parser.pressure_data, pattern, "camber on pressure")
